f813b1af48e05ed1ff5d7cc4720e8dd74498cf0b
Lucas Heredia <lucasheredia5475@gmail.com>
2022-10-13 16:42:18 -0300

    creo clase epicdouble

---
+++ TP2/src/main/java/EpicDoubleHashMap.java
@@ -0,0 +1,11 @@
+public class EpicDoubleHashMap <K extends Number, V, T> {
+    K number;
+    V object;
+    T object2;
+
+    public EpicDoubleHashMap(K number, V object, T object2) {
+        this.number = number;
+        this.object = object;
+        this.object2 = object2;
+    }
+}
