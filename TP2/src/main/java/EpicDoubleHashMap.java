bff1ef90fae89726f0883dfac44fc5444f20cf8f
Lucas Heredia <lucasheredia5475@gmail.com>
2022-10-16 17:17:52 -0300

    Agrego metodos

--- TP2/src/main/java/EpicDoubleHashMap.java
+++ TP2/src/main/java/EpicDoubleHashMap.java
@@ -1,11 +1,28 @@
-public class EpicDoubleHashMap <K extends Number, V, T> {
-    K number;
-    V object;
-    T object2;
+import java.util.HashMap;
+import java.util.Map;
 
-    public EpicDoubleHashMap(K number, V object, T object2) {
-        this.number = number;
-        this.object = object;
-        this.object2 = object2;
+public class EpicDoubleHashMap <K extends Number, V, T> {
+    HashMap<K, V> map1;
+    HashMap<K, T> map2;
+
+    public EpicDoubleHashMap(){
+        map1= new HashMap<>();
+        map2= new HashMap<>();
     }
+
+    public void addFirstType(K key, V value){
+        map1.put(key, value);
+    }
+
+    public void addSecondType(K key, T value){
+        map2.put(key, value);
+    }
+
+    public void addTwo(K key, V value, T value2){
+        map1.put(key,value);
+        map2.put(key,value2);
+    }
+
+    
+
 }
