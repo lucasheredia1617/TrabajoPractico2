10046bb9557c2cf62d6ce96924e74e60a9bec8f6
Gabriel Moncerrat <gabrielmoncerrat@gmail.com>
2022-10-20 18:39:16 -0300

    Static void main

--- TP2/src/main/java/Main.java
+++ TP2/src/main/java/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+	public static void main(String[] args) {
+        EpicDoubleHashMap<Integer, Integer, String> map = new EpicDoubleHashMap<>();
+        map.addFirstType(5, "Hola");
+        map.addSecondType(6, "Chau");
+        map.addThirdType(7, 6, "Hola");
+    }
 }
